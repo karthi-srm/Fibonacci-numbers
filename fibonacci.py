8df3d5ae197344745798d57f8a6c9c8b36200bc8
Karthikeyan M <mohankarthick444@gmail.com>
2025-11-24 21:27:26 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+
+def fibonacci(n):
+    fib_seq = []
+    a, b = 0, 1
+    for _ in range(n):
+        fib_seq.append(a)
+        a, b = b, a + b
+    return fib_seq
+num_terms = int(input("Enter the number of Fibonacci terms to display: "))
+result = fibonacci(num_terms)
+print("Fibonacci sequence:", result)
